518879bd345421f55a13ca12ce5fad28bdf61bb6
Parmis Samsami <parmis82samsami@gmail.com>
2023-09-24 10:30:49 +0330

    Add files via upload

---
+++ coffeshop.sql
@@ -0,0 +1,7 @@
+USE [master]
+GO
+
+/****** Object:  Database [Coffeeshop]    Script Date: 9/12/2023 3:43:48 PM ******/
+DROP DATABASE [Coffeeshop]
+GO
+
---
+++ universiry.sql
@@ -0,0 +1,7 @@
+USE [master]
+GO
+
+/****** Object:  Database [university]    Script Date: 9/12/2023 3:43:11 PM ******/
+DROP DATABASE [university]
+GO
+
