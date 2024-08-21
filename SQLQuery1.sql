ba03799e9e44f877181caf59dc9d0e3f5d9a3277
Parmis Samsami <parmis82samsami@gmail.com>
2024-08-21 15:11:16 +0330

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,8 +0,0 @@
-create database university
-create table STT(stid int not null,stname varchar(30) not null,stmjr varchar(30) not null, stdeg varchar(30) not null,primary key(stid))
-select * from STT
-insert into STT values(9998,'ali ahmadi','computer','M.S')
-select stid from stt where stname like 'm%'
-update stt set stname='ali rahmati' where stid=9998
-create table STCOT(stid int not null,coid int not null,term tinyint not null, year varchar(30) not null,foreign key(stid) references STT,foreign key(coid) references COT)
-
